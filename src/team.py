ca0b0915a7110126b20baaeed3da4e34ce804674
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-25 14:24:06 +0100

    passes first test

--- src/team.py
+++ src/team.py
@@ -1,5 +1,5 @@
 class Team:
     def __init__(self, team_name, players, coach):
-        self.team_name = team_name
+        self.name = team_name
         self.players = players
         self.coach = coach
